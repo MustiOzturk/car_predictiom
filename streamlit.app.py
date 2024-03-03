e10d240b6b9023b015a5db2344ddc5b3fc3230d8
MustiOzturk <161951943+MustiOzturk@users.noreply.github.com>
2024-03-03 13:50:12 +0300

    Add files via upload

---
+++ streamlit.app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title('Araba Tahmin Uygulaması')
+st.header('Arabanızın Fiyatını Tahmin Edin!')
+st.subheader('Bu uygulamayı kullanarak arabanızın fiyatını öğrenebilirsiniz')
